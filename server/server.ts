0726a6e446aaceb65058b80dbfce788e9759e9a3
Kaspar Bergert <kaspar.bergert@techno.ee>
2026-06-01 16:17:52 +0300

    add api routes

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import { createApp } from './app.ts';
+import router from './routes.ts';
 
 const port = Number(process.env.PORT ?? 3000);
 const app = createApp();
+
+app.use('/api', router);
 
 const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
